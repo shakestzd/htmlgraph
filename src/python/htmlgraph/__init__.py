02c647bcd95ac796a7f786c0b0df2d81f432170c
Shakes-tzd <shakestzd@gmail.com>
2026-03-21 04:20:32 -0400

    chore: bump version to 0.34.0

--- src/python/htmlgraph/__init__.py
+++ src/python/htmlgraph/__init__.py
@@ -138,7 +138,7 @@ from htmlgraph.types import (
 )
 from htmlgraph.work_type_utils import infer_work_type, infer_work_type_from_id
 
-__version__ = "0.33.96"
+__version__ = "0.34.0"
 __all__ = [
     # Exceptions
     "HtmlGraphError",
